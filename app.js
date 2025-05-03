da796c2cf86f138b73f1319f3feb8a4fd15ff719
Ayush Mathur <68462920+Ayush919@users.noreply.github.com>
2025-05-04 00:11:31 +0530

    Update cors policies in app.js

--- app.js
+++ app.js
@@ -12,7 +12,10 @@ const app = express();
 
 // Apply middlewares
 app.use(helmet());
-app.use(cors());
+app.use(cors({
+    origin: "https://insyd-notification-client-service.vercel.app/",
+    methods: "*", // ✅ Allow all HTTP verbs
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
